d81e2415f063fd7b9430fb46d4ece89b00f58c26
Greg <gregmarczak@outlook.com>
2017-12-25 21:10:34 +0000

    3.10 connect post to user

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20171224194430) do
     t.text     "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_grams_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
